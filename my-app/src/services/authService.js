b82c2a245fc455c6b1ab4c3e9e535a641fef6906
Anjaliraghu123 <anjalifromkpm@gmail.com>
2026-03-27 20:43:06 +0530

    add the api changes

--- my-app/src/services/authService.js
+++ my-app/src/services/authService.js
@@ -1,23 +1,7 @@
-import axios from "axios";
+const API_URL = "https://backend-kg76.onrender.com";
 
-const API_URL = " https://backend-kg76.onrender.com/api/student"; // your backend port
+export const registerUser = (data) =>
+  axios.post(`${API_URL}/api/auth/register`, data);
 
-// Register
-export const registerUser = async (data) => {
-  try {
-    const response = await axios.post(`${API_URL}/api/register`, data);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-// Login
-export const loginUser = async (data) => {
-  try {
-    const response = await axios.post(`${API_URL}/api/login`, data);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
+export const loginUser = (data) =>
+  axios.post(`${API_URL}/api/auth/login`, data);
